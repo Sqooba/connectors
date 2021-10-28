d79ceeb596b4b23a4bcb2707c291af76f704debf
Axel Fahy <axelfahy@users.noreply.github.com>
2021-10-28 11:14:32 +0200

    [virustotal] add prometheus metrics (#7)
    * [virustotal] add prometheus metrics
    
    * [virustotal] improve error message when file not found on VirusTotal

--- internal-enrichment/virustotal/src/virustotal/virustotal.py
+++ internal-enrichment/virustotal/src/virustotal/virustotal.py
@@ -35,7 +35,7 @@ class VirusTotalConnector:
             "VIRUSTOTAL_MAX_TLP", ["virustotal", "max_tlp"], config
         )
 
-        self.client = VirusTotalClient(self._API_URL, token)
+        self.client = VirusTotalClient(self.helper, self._API_URL, token)
 
         # Cache to store YARA rulesets.
         self.yara_cache = {}
@@ -90,10 +90,15 @@ class VirusTotalConnector:
         )
 
     def _process_file(self, observable):
+        self.helper.metric_inc("run_count")
+        self.helper.metric_state("running")
+
         json_data = self.client.get_file_info(observable["observable_value"])
         if json_data is None:
-            raise ValueError("An error has occurred")
+            self.helper.metric_inc("client_error_count")
+            return "File not found on VirusTotal."
         if "error" in json_data:
+            self.helper.metric_inc("client_error_count")
             if json_data["error"]["code"] == "QuotaExceededError":
                 self.helper.log_error(
                     "Quota limit reached, dropping the query to avoid queuing"
@@ -161,6 +166,8 @@ class VirusTotalConnector:
                 id=final_observable["id"],
                 external_reference_id=external_reference["id"],
             )
+            # One for the external reference plus the number of tags.
+            self.helper.metric_inc("record_send", 1 + len(attributes["tags"]))
 
             if "crowdsourced_yara_results" in attributes:
                 self.helper.log_info("[VirusTotal] adding yara results to file.")
@@ -187,8 +194,13 @@ class VirusTotalConnector:
                         toId=yara["id"],
                         relationship_type="related-to",
                     )
+                self.helper.metric_inc("record_send", len(yaras))
 
+            self.helper.metric_state("idle")
             return "File found on VirusTotal, knowledge attached."
+
+        self.helper.metric_state("idle")
+        return "File not found on VirusTotal."
 
     def _process_message(self, data):
         entity_id = data["entity_id"]
@@ -203,6 +215,8 @@ class VirusTotalConnector:
             if marking_definition["definition_type"] == "TLP":
                 tlp = marking_definition["definition"]
         if not OpenCTIConnectorHelper.check_max_tlp(tlp, self.max_tlp):
+            self.helper.metric_inc("error_count")
+            self.helper.metric_state("stopped")
             raise ValueError(
                 "Do not send any data, TLP of the observable is greater than MAX TLP"
             )
@@ -210,4 +224,6 @@ class VirusTotalConnector:
 
     def start(self):
         """Start the main loop."""
+        # Set default state as `idle`.
+        self.helper.metric_state("idle")
         self.helper.listen(self._process_message)
