6acab110646039c3511d0dd70acad8885214741c
Yann Schoeni <35560499+yanover@users.noreply.github.com>
2022-05-13 13:23:23 +0200

    Feature/vmray enrichment connector (#23)
    * Initial commit
    * Add OpenCTI API configuration
    * Connection to OpenCTI, ES, RMQ successfull, data recovery after enrichment request
    * First ES query using SHA256
    * Start looping throught JSON file
    * Work on SCO generation
    * Add builder for logic segmentation
    * Rename client.py
    * Update import
    * rename file
    * Renamle EsClient.py
    * Clean useless code
    * Work on SCO functions
    * Work on bundle's logic
    * Add entry for SCO fields
    * vmray/builder.py
    * Work on process_file
    * Add hashes type
    * Work on format_hashes and create_relationship
    * Add x-open-cti custom entity
    * Add custom field to x-opencti-text
    * Add yara fetcher + docker file (#22)
    * Add yara fetcher + docker file
    * Address PR comments
    * Add test for report, bundle, email, ip, yara, url
    * Add constants for email, ip and url
    * Implement yara, email, url and ip
    * Add yara indicator for testing
    * Add content to test analysis
    * Test relationship and blacklisted domain
    * Error handling
    * Implement report.json conversion for testing purpose
    * Work on Report testing
    * Fix VTI loop
    * Fix VTI loop
    * Add missing fields to stix2 entities
    * Improve error handling
    * Update test report
    * Add more tests
    * Create OpenCti string value
    * Will be squash
    * Update OpenCtiText method
    * Work on SCOs validation
    * Remove useless file on remote
    * Cache yara rule instead of ruleset (#24)
    * Add tmp folder
    * Add full blacklist and method to parse url
    * Mock OpenctiConnectorHelper
    * Improve code readability using Black and Linter
    * Move test outside of vmray module folder
    * Work on Email object
    * Change create_domain behaviour, testing, black code
    * Fix wrong call on metric_inc function
    * Change ValueError message
    * Add sample to object_refs
    * PR's comment modifications
    * Add one liner for object_refs filling
    * Add one liner for object_refs filling
    ---
    Co-authored-by: ysc <yschoeni@open-systems.com>
    Co-authored-by: Jean Rossier <jean.rossier@gmail.com>
    Co-authored-by: Jean Rossier <jean@sqooba.io>

---
+++ internal-enrichment/vmray/src/vmray/yara_fetcher.py
@@ -0,0 +1,129 @@
+# -*- coding: utf-8 -*-
+"""Yara fetcher client module."""
+import json
+from typing import Any, Optional
+from pycti import OpenCTIConnectorHelper
+import requests
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
+import plyara
+import plyara.utils
+
+# Custom type to simulate a JSON format.
+JSONType = dict[str, Any]
+
+
+class YaraFetcher:
+    """Yara fetcher. Fetches Yara rules from VMRay"""
+
+    def __init__(
+        self, helper: OpenCTIConnectorHelper, vmray_url: str, vmray_api_key: str
+    ) -> None:
+        """Initialize Yara fetcher."""
+        self.helper = helper
+        self.url = vmray_url
+        self.headers = {"Authorization": "api_key " + vmray_api_key}
+        # Cache to store YARA rules. The key is: ruleset_id::rule_name.
+        self.yara_cache = {}
+        self.helper.log_info(f"VMRay URL: {vmray_url}")
+
+    def _query(self, url: str) -> Optional[JSONType]:
+        """
+        Query VMRay API
+        """
+        self.helper.log_info(f"[VMRay] _query(): {url}")
+        # Configure the adapter for the retry strategy.
+        retry_strategy = Retry(
+            total=3,
+            status_forcelist=[429, 500, 502, 503, 504],
+            method_whitelist=["HEAD", "GET", "OPTIONS"],
+        )
+        adapter = HTTPAdapter(max_retries=retry_strategy)
+        http = requests.Session()
+        http.mount("https://", adapter)
+        response = None
+        try:
+            response = http.get(url, headers=self.headers, verify=False)
+            response.raise_for_status()
+            return response.json()
+        except requests.exceptions.HTTPError as errh:
+            self.helper.log_error(f"[VMRay] Http error: {errh}")
+            self.helper.metric_inc("client_error_count")
+        except requests.exceptions.ConnectionError as errc:
+            self.helper.log_error(f"[VMRay] Error connecting: {errc}")
+            self.helper.metric_inc("client_error_count")
+        except requests.exceptions.Timeout as errt:
+            self.helper.log_error(f"[VMRay] Timeout error: {errt}")
+            self.helper.metric_inc("client_error_count")
+        except requests.exceptions.RequestException as err:
+            self.helper.log_error(f"[VMRay] Something else happened: {err}")
+        except json.JSONDecodeError as err:
+            self.helper.metric_inc("client_error_count")
+            self.helper.log_error(
+                f"[VMRay] Error decoding the json: {err} - {response.text}"
+            )
+        except Exception as err:
+            self.helper.log_error(f"[VMRay] Unknown error {err}")
+        # Try content failed, return None value
+        return None
+
+    def _get_yara_ruleset(self, ruleset_id: str) -> Optional[JSONType]:
+        """
+        Retrieve the YARA rules based on the given ruleset id.
+
+        Parameters
+        ----------
+        ruleset_id : str
+            Ruleset id to retrieve.
+
+        Returns
+        -------
+        JSON
+            YARA ruleset objects, as JSON.
+        """
+        url = f"{self.url}/rest/yara/{ruleset_id}"
+        return self._query(url)
+
+    def get_yara_rule(self, ruleset_id: str, rule_name: str) -> Optional[JSONType]:
+        """
+        Retrieve a Yara rule based on the ruleset id and rule name.
+
+        Parameters
+        ----------
+        ruleset_id : str
+            Ruleset id to retrieve.
+        rule_name : str
+            Rule name to retrieve.
+
+        Returns
+        -------
+        JSON
+            YARA rule object as JSON (as parsed by plyara).
+        """
+        # Lookup in the cache for the rule, otherwise, request VMRay API.
+        rule = None
+        rule_key = self.get_rule_key(ruleset_id, rule_name)
+        if rule_key in self.yara_cache:
+            self.helper.log_debug(f"Retrieving YARA rule {rule_key} from cache.")
+            rule = self.yara_cache[rule_key]
+        else:
+            self.helper.log_debug(f"Retrieving YARA rule {rule_key} from API.")
+            ruleset = self._get_yara_ruleset(ruleset_id)
+            # Parse the rules to store them in cache
+            parser = plyara.Plyara()
+            rules = parser.parse_string(ruleset["data"][0]["yara_ruleset_rules"])
+            for r in rules:
+                cache_key = self.get_rule_key(ruleset_id, r["rule_name"])
+                self.yara_cache[cache_key] = r
+                if r["rule_name"] == rule_name:
+                    rule = r
+
+        if rule is not None:
+            return rule
+        else:
+            self.helper.log_warning(f"No YARA rule for rule {rule_key}")
+            return None
+
+    @staticmethod
+    def get_rule_key(ruleset_id: str, rule_name: str) -> str:
+       return f"{ruleset_id}::{rule_name}"
