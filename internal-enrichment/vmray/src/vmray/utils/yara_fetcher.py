9e7db1981c96ae384c37508e8ff87f54161aca0c
Yann Schoeni <35560499+yanover@users.noreply.github.com>
2023-05-31 15:23:07 +0200

    [vmray] bypass pylara parsing error (#37)
    * Chunk yara-rules on parsing error

--- internal-enrichment/vmray/src/vmray/utils/yara_fetcher.py
+++ internal-enrichment/vmray/src/vmray/utils/yara_fetcher.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """Yara fetcher client module."""
 import json
-from typing import Any, Dict, Optional
+from typing import Any, Dict, Optional, List, Tuple
 
 import plyara
 import plyara.utils
@@ -9,6 +9,7 @@ import requests
 from pycti import OpenCTIConnectorHelper
 from requests.adapters import HTTPAdapter
 from urllib3.util import Retry
+from plyara import ParseTypeError
 
 # Custom type to simulate a JSON format.
 JSONType = Dict[str, Any]
@@ -24,6 +25,7 @@ class YaraFetcher:
         self.helper = helper
         self.url = vmray_url
         self.headers = {"Authorization": "api_key " + vmray_api_key}
+        self.parser = plyara.Plyara()
         # Cache to store YARA rules. The key is: ruleset_id::rule_name.
         self.yara_cache = {}
         self.helper.log_info(f"VMRay URL: {vmray_url}")
@@ -115,28 +117,82 @@ class YaraFetcher:
             * YARA rule object as JSON (as parsed by plyara)
         """
         # Lookup in the cache for the rule, otherwise, request VMRay API.
-        existing_rule = None
+        self.parser.clear()
         rule_key = self.get_rule_key(ruleset_id, rule_name)
-        if rule_key in self.yara_cache:
-            self.helper.log_debug(f"Retrieving YARA rule {rule_key} from cache.")
-            existing_rule = self.yara_cache[rule_key]
-        else:
-            self.helper.log_debug(f"Retrieving YARA rule {rule_key} from API.")
-            ruleset = self._get_yara_ruleset(ruleset_id)
-            # Parse the rules to store them in cache
-            parser = plyara.Plyara()
-            rules = parser.parse_string(ruleset["data"][0]["yara_ruleset_rules"])
-            for rule in rules:
-                cache_key = self.get_rule_key(ruleset_id, rule["rule_name"])
-                self.yara_cache[cache_key] = rule
-                if rule["rule_name"] == rule_name:
-                    existing_rule = rule
+        existing_rule = self.yara_cache.get(rule_key)
 
         if existing_rule is not None:
+            self.helper.log_debug(f"Retrieving YARA rule {rule_key} from cache.")
             return existing_rule
+
+        self.helper.log_debug(f"Retrieving YARA rule {rule_key} from API.")
+        ruleset_data = self._get_yara_ruleset(ruleset_id)["data"]
+
+        if ruleset_data is None:
+            self.helper.log_warning(f"No YARA rule for rule {rule_key}")
+            return None
+
+        rules, err_count = self.parse_yara_rule(
+            ruleset_data[0].get("yara_ruleset_rules", "")
+        )
+        self.helper.log_debug(
+            f"The yara parser returned {len(rules)} parsed rules and {err_count} error(s)"
+        )
+
+        matching_rule = None
+        for rule in rules:
+            cache_key = self.get_rule_key(ruleset_id, rule["rule_name"])
+            # Populate the cache
+            self.yara_cache[cache_key] = rule
+            if rule["rule_name"] == rule_name:
+                matching_rule = rule
+
+        if matching_rule is not None:
+            return matching_rule
 
         self.helper.log_warning(f"No YARA rule for rule {rule_key}")
         return None
+
+    def parse_yara_rule(self, ruleset: str) -> Tuple[List[str], int]:
+        """
+        Parse Yara rules based on the ruleset. Try to parse the whole ruleset first, split it rule by rule if it fails.
+
+        Parameters
+        ----------
+        ruleset: str
+            * Ruleset string to parse
+        Returns
+        -------
+        Tuple[List[str], int]
+            * A List of rules parsed
+            * The number of rule(s) skip due to error
+        """
+        try:
+            # Try to parse the entire rules file
+            return self.parser.parse_string(ruleset), 0
+        except ParseTypeError:
+            self.helper.log_info(
+                "The Yara file contains errors. It will be chunked and processed rule by rule."
+            )
+            parsed_rules = []
+            unparsed_rules = 0
+            # Split the rules
+            split = ruleset.split("rule ")
+            ruleset_split = ["rule " + rule if rule else "" for rule in split]
+            # Process each rule separately
+            self.helper.log_debug(f"Processing {len(ruleset_split)} yara rule(s)")
+            for r in ruleset_split:
+                try:
+                    rule_parsed = self.parser.parse_string(r)
+                    if rule_parsed:
+                        parsed_rules.extend(rule_parsed)
+                except ParseTypeError:
+                    self.helper.log_error(
+                        f"The rule {r} could not be parsed, skipping.."
+                    )
+                    unparsed_rules += 1
+                    continue
+            return parsed_rules, unparsed_rules
 
     @staticmethod
     def get_rule_key(ruleset_id: str, rule_name: str) -> str:
