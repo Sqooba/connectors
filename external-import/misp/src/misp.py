9b4a4a91469a1ca053900547d440c6af6ef581db
Florent <florent@sqooba.io>
2023-09-07 14:02:49 +0200

    (first)

--- external-import/misp/src/misp.py
+++ external-import/misp/src/misp.py
@@ -169,6 +169,12 @@ class Misp:
             config,
             default=False,
         )
+        self.keep_original_tags_as_label = get_config_variable(
+            "MISP_KEEP_ORIGINAL_TAGS",
+            ["misp", "keep_original_tags_as_label"],
+            config,
+            default="",
+        ).split(",")
         self.misp_enforce_warning_list = get_config_variable(
             "MISP_ENFORCE_WARNING_LIST",
             ["misp", "enforce_warning_list"],
@@ -2187,7 +2193,11 @@ class Misp:
             return opencti_tags
 
         for tag in tags:
-            if (
+            # we take the tag as-is if it starts by a prefix stored in the keep_original_tags_as_label configuration
+            if any(map(lambda s: s.startswith(tag), self.keep_original_tags_as_label)):
+                opencti_tags.append(tag)
+
+            elif (
                 tag["name"] != "tlp:white"
                 and tag["name"] != "tlp:green"
                 and tag["name"] != "tlp:amber"
