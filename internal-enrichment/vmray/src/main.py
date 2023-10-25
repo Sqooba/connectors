d572f8c5cb1ca4c9715764a47bd19cabc79543f0
sqooba_yann <sqooba_yann@knight.zoo>
2023-10-25 14:38:06 +0200

    Fix config file path

--- internal-enrichment/vmray/src/main.py
+++ internal-enrichment/vmray/src/main.py
@@ -5,6 +5,6 @@ from pathlib import Path
 from vmray import VMRayConnector
 
 if __name__ == "__main__":
-    config_file = Path(__file__).parent.parent.resolve() / "config.yml"
+    config_file = Path(__file__).parent.resolve() / "config.yml"
     connector = VMRayConnector(config_file)
     connector.start()
